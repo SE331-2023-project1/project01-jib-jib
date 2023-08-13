1732f1f3553655e319342ad3e0f97e05eab42569
Putipong Sailen <tyice789@gmail.com>
2023-08-13 12:58:47 +0700

    add AdviserType

--- project-compo-mid/src/type.ts
+++ project-compo-mid/src/type.ts
@@ -6,3 +6,12 @@ export interface StudentItem {
   profileImage: string
   courses: string[]
 }
+
+export interface AdviserItem {
+  id: number
+  name: string
+  surname: string
+  professorID: number
+  profileImage: string
+  courses: string[]
+}
