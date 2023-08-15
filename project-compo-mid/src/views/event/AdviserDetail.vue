de691330848ca200409346c223c1ada283f1ec4e
nureman <nureman_s@cmu.ac.th>
2023-08-15 14:00:20 +0700

    add comment box

--- project-compo-mid/src/views/event/AdviserDetail.vue
+++ project-compo-mid/src/views/event/AdviserDetail.vue
@@ -1,19 +1,35 @@
 <script setup lang="ts">
-import { type AdviserItem } from '@/type';
-import type { PropType } from 'vue';
-
-defineProps({
-    professer: {
-        type: Object as PropType<AdviserItem>,
-            require: true
-    }
+import { type AdviserItem } from '@/type'
+import { ref } from 'vue'
+import type { PropType } from 'vue'
+import { useRouter } from 'vue-router'
+const props = defineProps({
+  professer: {
+    type: Object as PropType<AdviserItem>,
+    require: true
+  }
 })
-
+import { useMessageStore } from '@/stores/message'
+const store = useMessageStore()
+const comment = ref('')
+const router = useRouter()
+function updateComment() {
+  if (props.professer && props.professer.id) {
+    store.updateMessage(String(props.professer?.id), comment.value)
+    router.push({
+      name: 'student-detail',
+      params: {
+        id: props.professer?.id
+      }
+    })
+  }
+}
 </script>
 
 <template>
-    <div v-if="professer">
-        <h1>Adviser {{ professer.name }}</h1>
-        
-    </div>
+  <div v-if="professer">
+    <h1>Adviser {{ professer.name }}</h1>
+    <input type="text" placeholder="Enter comment .." v-model="comment" />
+    <button @click="updateComment">Submit</button>
+  </div>
 </template>
--- project-compo-mid/src/views/event/StudentDetail.vue
+++ project-compo-mid/src/views/event/StudentDetail.vue
@@ -1,24 +1,33 @@
 <script setup lang="ts">
-import { type StudentItem } from '@/type';
-import type { PropType } from 'vue';
-import ReviewForm from '@/views/'
-
+import { type StudentItem } from '@/type'
+import { computed, type PropType } from 'vue'
+import { useMessageStore } from '@/stores/message'
+import { useRouter } from 'vue-router'
+const store = useMessageStore()
 defineProps({
-    student: {
-        type: Object as PropType<StudentItem>,
-        require: true
-    }
+  student: {
+    type: Object as PropType<StudentItem>,
+    require: true
+  }
 })
-
+const router = useRouter()
+const studentId = router.currentRoute.value.params.id
+const studentComment = computed(() => {
+  return store.getCommentForStudent(String(studentId))
+})
 </script>
 
 <template>
-    <div v-if="student" class="font-mono grid grid-cols-2 ">
-        <p class="font-semibold text-left">Name: </p>
-        <p class="text-center"> {{ student.name }}</p>
-        <p class="font-semibold text-left">SurName: </p>
-        <p class="text-center"> {{ student.surname }}</p>
-        <p class="font-semibold text-left">Course: </p>
-        <p class="text-center"> {{ student.courses.toString() }}</p>
+  <div v-if="student" class="font-mono grid grid-cols-2">
+    <p class="font-semibold text-left">Name:</p>
+    <p class="text-center">{{ student.name }}</p>
+    <p class="font-semibold text-left">SurName:</p>
+    <p class="text-center">{{ student.surname }}</p>
+    <p class="font-semibold text-left">Course:</p>
+    <p class="text-center">{{ student.courses.toString() }}</p>
+    <div class="message">
+      <h1>Comment from Advisor</h1>
+      <p>{{ studentComment }}</p>
     </div>
+  </div>
 </template>
