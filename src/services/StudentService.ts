20a4f4fbc9d972b2c0b81395b78f0238a13e439d
nureman <nureman_s@cmu.ac.th>
2023-08-16 14:13:43 +0700

    set up environment

--- src/services/StudentService.ts
+++ src/services/StudentService.ts
@@ -1,27 +1,36 @@
-import axios from 'axios'
-import type { AxiosInstance, AxiosResponse } from 'axios'
-import type { StudentItem, AdviserItem } from '@/type'
-
+import axios from "axios";
+import type { AxiosInstance, AxiosResponse } from "axios";
+import type { StudentItem, AdviserItem } from "@/type";
 
 const apiClient: AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3006',
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: false,
   headers: {
-    Accept: 'application/json',
-    'Content-Type': 'application/json'
-  }
-})
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
 export default {
-  getStudent(perPage: number, page: number): Promise<AxiosResponse<StudentItem[]>> {
-    return apiClient.get<Array<StudentItem>>('/students?_limit=' + perPage + '&_page=' + page)
+  getStudent(
+    perPage: number,
+    page: number
+  ): Promise<AxiosResponse<StudentItem[]>> {
+    return apiClient.get<Array<StudentItem>>(
+      "/students?_limit=" + perPage + "&_page=" + page
+    );
   },
   getStudentById(id: number): Promise<AxiosResponse<StudentItem>> {
-    return apiClient.get<StudentItem>('/students/' + id.toString())
+    return apiClient.get<StudentItem>("/students/" + id.toString());
   },
-  getAdviser(perPage: number, page: number): Promise<AxiosResponse<AdviserItem[]>> {
-    return apiClient.get<Array<AdviserItem>>('/professer?_limit=' + perPage + '&_page=' + page)
+  getAdviser(
+    perPage: number,
+    page: number
+  ): Promise<AxiosResponse<AdviserItem[]>> {
+    return apiClient.get<Array<AdviserItem>>(
+      "/professer?_limit=" + perPage + "&_page=" + page
+    );
   },
   getAdviserById(id: number): Promise<AxiosResponse<AdviserItem>> {
-    return apiClient.get<AdviserItem>('/professer/' + id.toString())
-  }
-}
+    return apiClient.get<AdviserItem>("/professer/" + id.toString());
+  },
+};
