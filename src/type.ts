41ba7c5a08c47cd1603bb720c9de82f094a90934
nureman <nureman_s@cmu.ac.th>
2023-08-16 09:53:17 +0700

    delete some data in db

--- src/type.ts
+++ src/type.ts
@@ -1,18 +1,17 @@
 export interface StudentItem {
-  id: number
-  name: string
-  surname: string
-  studentID: number
-  profileImage: string
-  courses: string[]
-  professorId: number
+  id: number;
+  name: string;
+  surname: string;
+  studentID: number;
+  profileImage: string;
+  courses: string[];
+  professorId: number;
 }
 
 export interface AdviserItem {
-  id: number
-  name: string
-  surname: string
-  professorID: number
-  profileImage: string
-  courses: string[]
+  id: number;
+  name: string;
+  surname: string;
+  professorID: number;
+  profileImage: string;
 }
