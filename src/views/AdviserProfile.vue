212a4b6790c52471cda849bc9055471956842931
nureman <nureman_s@cmu.ac.th>
2023-08-21 16:14:01 +0700

    move prof detail box

--- src/views/AdviserProfile.vue
+++ src/views/AdviserProfile.vue
@@ -1,33 +1,62 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import type { AdviserItem } from '@/type';
-import StudentService from '@/services/StudentService';
-const professer = ref<AdviserItem | null>(null)
+import { computed, ref, type PropType } from "vue";
+import type { AdviserItem } from "@/type";
+import StudentService from "@/services/StudentService";
+import { useMessageStore } from "@/stores/message";
+const professer = ref<AdviserItem | null>(null);
 const props = defineProps({
-    id: String
-})
+  id: String,
+  professer: {
+    type: Object as PropType<AdviserItem>,
+    require: true,
+  },
+});
 
 StudentService.getAdviserById(Number(props.id))
-    .then((response) => {
-        professer.value = response.data
-    })
-    .catch((error) =>{
-        console.log(error)
-    })
+  .then((response) => {
+    professer.value = response.data;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
 
+const store = useMessageStore();
+const detail = computed(() => store.getDetail(String(props.professer?.id)));
+const newDetail = ref("");
+function addDetail() {
+  store.addDetail(String(props.professer?.id), newDetail.value);
+  newDetail.value = "";
+}
 </script>
 
 <template>
-    <div class="my-10">
-        <h1 class="text-center text-3xl">Professer ID: {{  professer?.professorID }}</h1>
-        <div class="my-5">
-            <div class="justify-center my-auto grid gap-3">
-                <img :src="professer?.profileImage" alt="Student Image" class="h-48 w-36" />
-                <div class="font-mono text-left">
-                    <p>Name: {{ professer?.name }} {{ professer?.surname }}</p>
-                </div>
-            </div>
+  <div class="my-10">
+    <h1 class="text-center text-3xl">
+      Professer ID: {{ professer?.professorID }}
+    </h1>
+    <div class="my-5">
+      <div class="justify-center my-auto grid gap-3">
+        <img
+          :src="professer?.profileImage"
+          alt="Student Image"
+          class="h-48 w-36"
+        />
+        <div class="font-mono text-left">
+          <p>Name: {{ professer?.name }} {{ professer?.surname }}</p>
         </div>
+        <div v-if="detail && detail.length">
+          <h1>professer Deatail</h1>
+          <p v-for="(details, index) in detail" :key="index">{{ details }}</p>
+        </div>
+        <div class="addDetail">
+          <input
+            type="text"
+            v-model="newDetail"
+            placeholder="Add Professer Detail.."
+          />
+          <button @click="addDetail">Add Detail</button>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
-
--- src/views/event/AdviserDetail.vue
+++ src/views/event/AdviserDetail.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { type AdviserItem, type StudentItem } from "@/type";
-import { computed, ref, type PropType } from "vue";
+import { ref, type PropType } from "vue";
 import { useMessageStore } from "@/stores/message";
 import router from "@/router";
 const props = defineProps({
@@ -23,16 +23,10 @@ function onSubmit() {
     name: "student-detail",
   });
 }
-const detail = computed(() => store.getDetail(String(props.professer?.id)));
-const newDetail = ref("");
-function addDetail() {
-  store.addDetail(String(props.professer?.id), newDetail.value);
-  newDetail.value = "";
-}
 </script>
 
 <template>
-  <div class="grid grid-cols-2">
+  <div class="grid grid-cols-2 p-5">
     <div class="mx-auto">
       <img
         :src="professer?.profileImage"
@@ -40,36 +34,26 @@ function addDetail() {
         class="h-48 w-36"
       />
     </div>
+
     <div class="font-mono text-center">
       <div class="grid grid-cols-2 gap-2">
         <p>Name-Surname:</p>
         <p>{{ professer?.name }} {{ professer?.surname }}</p>
         <p>ProfesserID:</p>
         <p>{{ professer?.professorID }}</p>
-        <div v-if="detail && detail.length">
-          <h1>professer Deatail</h1>
-          <p v-for="(details, index) in detail" :key="index">{{ details }}</p>
-        </div>
-        <p>{{ student?.name }}</p>
-        <div class="addDetail">
-          <input
-            type="text"
-            v-model="newDetail"
-            placeholder="Add Professer Detail.."
-          />
-          <button @click="addDetail">Add Detail</button>
-        </div>
+        <p>Student Name:</p>
+        <p>{{ student?.name }} {{ student?.surname }}</p>
       </div>
+      <div class="m-5">
+        <input type="text" placeholder="Enter comment .." v-model="comment" />
+        <button @click="onSubmit">Submit</button>
+      </div>
+      <RouterLink
+        :to="{ name: 'professer-profile', params: { id: professer?.id } }"
+        class="font-extrabold text-red-700 font-mono"
+        >Click to see more details</RouterLink
+      >
     </div>
   </div>
-  <div>
-    <RouterLink
-      :to="{ name: 'professer-profile', params: { id: professer?.id } }"
-      >More Details</RouterLink
-    >
-  </div>
-  <div class="flex justify-center my-5">
-    <input type="text" placeholder="Enter comment .." v-model="comment" />
-    <button @click="onSubmit">Submit</button>
-  </div>
+  <div></div>
 </template>
