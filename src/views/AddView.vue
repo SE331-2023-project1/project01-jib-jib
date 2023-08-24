5e6975e9434d9741d084c6c4d326adbb1d0b7ce2
nureman <nureman_s@cmu.ac.th>
2023-08-24 22:31:43 +0700

    Add alert message

--- src/views/AddView.vue
+++ src/views/AddView.vue
@@ -2,8 +2,12 @@
 import { ref } from "vue";
 import { useAdviserStore } from "@/stores/newAdviser";
 import type { AdviserItem } from "@/type";
-
-
+import { useMessageStore } from "@/stores/message";
+import { storeToRefs } from "pinia";
+import { useRouter } from "vue-router";
+const store = useMessageStore();
+const { flashMessage } = storeToRefs(store);
+const router = useRouter();
 const adviserStore = useAdviserStore();
 const Iadviser = ref<AdviserItem>({
   id: 0,
@@ -15,36 +19,76 @@ const Iadviser = ref<AdviserItem>({
   email: "",
 });
 function addAdviser() {
-  adviserStore.addTemporaryAdviser(Iadviser.value);
+  if (
+    Iadviser.value.name &&
+    Iadviser.value.surname &&
+    Iadviser.value.professorID &&
+    Iadviser.value.profileImage
+  ) {
+    adviserStore.addTemporaryAdviser(Iadviser.value);
+    store.updateflashcard("Advisoer Data alraedy update");
+    setTimeout(() => {
+      store.resetflashcard();
+      router.push({
+        name: "professer",
+      });
+    }, 2000);
+  } else {
+    alert("Please fill in all the required fields before adding.");
+  }
 }
 </script>
 
 <template>
   <div class="flex justify-center">
     <img
-          alt="Vue logo"
-          src="https://logolook.net/wp-content/uploads/2021/11/Hogwarts-Logo.png"
-          class="mt-5 h-40 w-70"
-        />
+      alt="Vue logo"
+      src="https://logolook.net/wp-content/uploads/2021/11/Hogwarts-Logo.png"
+      class="mt-5 h-40 w-70"
+    />
   </div>
   <div class="mx-20 my-10 text-center">
     <div class="my-10">
-      <p class="text-2xl font-mono font-bold text-center">Add Adviser Details</p>
+      <p class="text-2xl font-mono font-bold text-center">
+        Add Adviser Details
+      </p>
     </div>
-    <div class="flex justify-center ">
+    <div class="flex justify-center">
       <div class="grid grid-cols-2 gap-2 font-mono">
-      <p class="text-left">Profresser-ID: </p>
-      <input v-model="Iadviser.professorID" placeholder="Add ProfresserID..." class="text-center border-2 "/>
-      <p class="text-left">Name:</p>
-      <input v-model="Iadviser.name" placeholder="Add Name..." class="text-center border-2">
-      <p class="text-left">SurName:</p>
-      <input v-model="Iadviser.surname" placeholder="Add SurName..." class="text-center border-2"/>
-      <p class="text-left">Image-URL: </p>
-      <input v-model="Iadviser.profileImage" placeholder="Add Image-URL..." class="text-center border-2"/>
-    </div>
+        <p class="text-left">Profresser-ID:</p>
+        <input
+          v-model="Iadviser.professorID"
+          placeholder="Add ProfresserID..."
+          class="text-center border-2"
+        />
+        <p class="text-left">Name:</p>
+        <input
+          v-model="Iadviser.name"
+          placeholder="Add Name..."
+          class="text-center border-2"
+        />
+        <p class="text-left">SurName:</p>
+        <input
+          v-model="Iadviser.surname"
+          placeholder="Add SurName..."
+          class="text-center border-2"
+        />
+        <p class="text-left">Image-URL:</p>
+        <input
+          v-model="Iadviser.profileImage"
+          placeholder="Add Image-URL..."
+          class="text-center border-2"
+        />
+        <p class="text-center text-blue-600">{{ flashMessage }}</p>
+      </div>
     </div>
     <div class="text-center m-8">
-      <button @click="addAdviser" class="border-2 border-black bg-red-800 font-mono text-white p-2">Add Student</button>
+      <button
+        @click="addAdviser"
+        class="border-2 border-black bg-red-800 font-mono text-white p-2"
+      >
+        Add Adviser
+      </button>
     </div>
   </div>
 </template>
